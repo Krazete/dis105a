81a21a567891e7d415d2b4191ee69b7508d10364
Ted Michael Celis <krazete@gmail.com>
2017-07-18 11:29:51 -0700

    local files displayed locally, removed redirection

--- main.py
+++ main.py
@@ -3,27 +3,12 @@ from google.appengine.api import urlfetch
 import re
 
 class Index(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        # original website
-        original = 'https://www.math.uci.edu/~aaronc8/Su2017Math105A.html'
+    def set(self, path, header):
+        # set header
+        self.response.headers['Content-Type'] = header
         try:
-            # get original website's content
-            content = urlfetch.fetch(original).content
-            # replace a bunch of tags and styles and get rid of redundant whitespaces
-            content += '<script>\n\
-document.body.parentElement.innerHTML = document.body.parentElement.innerHTML\n\
-.replace(/<\\/p>/g, "")\n\
-.replace(/(<br\\s*\\/*>\\s*){2,}/g, "<br>")\n\
-.replace(/\\s\\s+/g, " ")\n\
-.replace(/<b>/g, "<h3>")\n\
-.replace(/<\\/b>\\s*(<br>)*/g, "</h3>")\n\
-.replace(/<\\/*font.*?>/g, "");\n\
-document.body.style.backgroundColor = "#def";\n\
-var ico = document.head.appendChild(document.createElement("link"));\n\
-ico.rel="icon";\n\
-ico.href="./favicon.ico";\n\
-</script>'
+            # get original content
+            content = urlfetch.fetch(path).content
         except:
             # redirect upon failure
             content = '<!DOCTYPE html>\n\
@@ -50,37 +35,44 @@ ico.href="./favicon.ico";\n\
         <a href="{0}">Click here if it\'s taking too long.</a>\n\
     </span>\n\
 </body>'.format(original)
-        # display the revised content
+        # return the content
+        return content
+    def get(self):
+        # original website
+        path = 'https://www.math.uci.edu/~aaronc8/Su2017Math105A.html'
+        # get original website content
+        content = self.set(path, 'text/html')
+        # replace a bunch of tags and styles and get rid of redundant whitespaces
+        content += '<script>\n\
+document.body.parentElement.innerHTML = document.body.parentElement.innerHTML\n\
+.replace(/<\\/p>/g, "")\n\
+.replace(/(<br\\s*\\/*>\\s*){2,}/g, "<br>")\n\
+.replace(/\\s\\s+/g, " ")\n\
+.replace(/<b>/g, "<h3>")\n\
+.replace(/<\\/b>\\s*(<br>)*/g, "</h3>")\n\
+.replace(/<\\/*font.*?>/g, "");\n\
+document.body.style.backgroundColor = "#def";\n\
+var ico = document.head.appendChild(document.createElement("link"));\n\
+ico.rel="icon";\n\
+ico.href="./favicon.ico";\n\
+</script>'
+        # display revised content
         self.response.write(content)
 
-class Local(webapp2.RequestHandler):
+class Local(Index):
     def get(self, path):
-        self.response.headers['Content-Type'] = 'text/html'
-        # get original website's root
-        root = 'https://www.math.uci.edu/~aaronc8/'
-        # redirect to original file
-        self.response.write('<!DOCTYPE html>\n\
-<head>\n\
-    <title>Math 105A Discussion</title>\n\
-    <style>\n\
-        span {{\n\
-            position: absolute;\n\
-            top: 50%;\n\
-            left: 50%;\n\
-            transform: translate(-50%, -50%);\n\
-            text-align: center;\n\
-            vertical-align: middle;\n\
-        }}\n\
-    </style>\n\
-    <meta http-equiv="refresh" content="0;url={0}">\n\
-</head>\n\
-<body>\n\
-    <span>\n\
-        You should be redirected to the original file soon.\n\
-        <br>\n\
-        <a href="{0}">Click here if it\'s taking too long.</a>\n\
-    </span>\n\
-</body>'.format(root + path))
+        # get original website
+        path = 'https://www.math.uci.edu/~aaronc8/' + path
+        # set header based on extension
+        ext = path.split('.')[-1]
+        if ext == 'pdf':
+            header = 'application/' + ext
+        else:
+            header = 'text/' + ext
+        # get original website content
+        content = self.set(path, header)
+        # display original content
+        self.response.write(content)
 
 sitemap = [
     ('/', Index),
